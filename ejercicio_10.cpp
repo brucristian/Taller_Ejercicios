c0782ccc6ddc89cb64ed951d4a03bed14d1f0608
joan566 <joansebastianvergaravalencia@gmail.com>
2025-09-10 20:23:22 -0500

    Update ejercicio_10.cpp

--- ejercicio_10.cpp
+++ ejercicio_10.cpp
@@ -1,3 +1,10 @@
+/*
+Joan Sebastian Vergara Valencia Codigo: 6902510055;
+Josue Villasmil Codigo: 6902510017
+Cristian Bru Codigo: 6902510029
+Armando Escobar Codigo: 6902510023
+*/
+
 #include <iostream>
 using namespace std;
 

ca918a4fb5969d65b6a0e8c5c1636005129494e0
armandoe02 <armandoescobargentile@gmail.com>
2025-09-17 15:03:57 -0500

    Update ejercicio_4.cpp

--- ejercicio_4.cpp
+++ ejercicio_4.cpp
@@ -18,8 +18,14 @@ int numeroMayor(int x, int y) {
 
 int main() {
 	int x, y;
-	cout << "Digite dos numeros enteros: "; cin >> x >> y;
+	cout << "Digite dos numeros enteros " << endl; 
+	cout << "Ingrese el primer numero: ";
+	cin >> x;
+	cout << "Ingrese el segundo numero: ";
+	cin >> y;
 	
 	cout << "El numero mayor entre " << x << " y " << y << " es: " << numeroMayor(x, y);
-	
+
+
+	return 0;
 }
